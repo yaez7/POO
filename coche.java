3fd85bf86aae68a3d529f2eaffd05c60b6d8b927
yaez7 <160163549+yaez7@users.noreply.github.com>
2024-03-07 12:26:53 -0600

    Create coche.java

---
+++ coche.java
@@ -0,0 +1,22 @@
+import java.io*;
+class Coche
+{
+//atributos
+string color;
+string marca;
+int km;
+//metodo
+public static void main (String[]arg)
+{
+//objeto
+Coche coche1 = new Coche();
+//modicar atributos
+Coche.color="Rojo"
+Coche.marca="mitsubishi";
+Coche1.km=0;
+// valores del objetivo
+System.out.print("El color del coche es: "+Coche.color);
+System.out.print("La marca es: "+Coche.marca);
+System.out.print("El km es: "+Coche1.km);
+ }
+}
